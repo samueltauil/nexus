29d349f60079cffd6e60780d93f6e80f42e90728
Samuel Tauil <samueltauil@gmail.com>
2018-02-02 10:37:10 -0500

    push artifacts to nexus

---
+++ nexusupload.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# copy and run this script to the root of the repository directory containing files
+# this script attempts to exclude uploading itself explicitly so the script name is important
+# Get command line params
+while getopts ":r:u:p:" opt; do
+	case $opt in
+		r) REPO_URL="$OPTARG"
+		;;
+		u) USERNAME="$OPTARG"
+		;;
+		p) PASSWORD="$OPTARG"
+		;;
+	esac
+done
+
+find . -type f -not -path './mavenimport\.sh*' -not -path '*/\.*' -not -path '*/\^archetype\-catalog\.xml*' -not -path '*/\^maven\-metadata\-local*\.xml' -not -path '*/\^maven\-metadata\-deployment*\.xml' | sed "s|^\./||" | xargs -I '{}' curl -u "$USERNAME:$PASSWORD" -X PUT -v -T {} ${REPO_URL}/{} ;
--- provision.sh
+++ provision.sh
@@ -3,8 +3,8 @@
 oc new-app https://github.com/samueltauil/nexus
 
 oc expose svc nexus
-sleep 30
+sleep 40
 url=$(oc get routes | egrep 'nexus' | awk '{print $2}')
 export NEXUS_BASE_URL=$url
-sleep 220
+sleep 260
 ./addjbossrepos.sh
